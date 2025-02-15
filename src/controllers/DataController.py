38419140cd00f304cbc06c4d1ff55699411ee331
Mariam Ahmed <mariamalkaseh741@gmail.com>
2025-02-16 00:08:37 +0200

    .

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,6 @@
-from BaseController import BaseController
+from .BaseController import BaseController
 from fastapi import UploadFile # type: ignore
-
+from models import ResponseSignal
 class DataController(BaseController):
 
     def __init__(self):
@@ -9,7 +9,7 @@ class DataController(BaseController):
 
     def validate_uploaded_file(self, file: UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False
-        return True
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
+        return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile # type: ignore
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status # type: ignore
+from fastapi.responses import JSONResponse # type: ignore
 import os
 from helper.config import get_settings, Settings
 from controllers import DataController
@@ -9,9 +10,17 @@ data_router = APIRouter(
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data(project_id: str, file: UploadFile,
+async def upload_data(project_id: int, file: UploadFile,
                       apee_settings: Settings = Depends(get_settings)):
     # validate the file properties
-    is_valid = DataController().validate_uploaded_file(file=file)
+    is_valid, result_signal = DataController().validate_uploaded_file(file=file)
 
-    return is_valid
+    return is_valid, result_signal
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
