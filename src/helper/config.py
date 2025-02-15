73ab04bcdce4658ae8ba2cce10fce688b704b8fd
Mariam Ahmed <mariamalkaseh741@gmail.com>
2025-02-15 22:57:54 +0200

    update.controllers

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
 
     
 
---
+++ src/routes/data.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile # type: ignore
+import os
+from helper.config import get_settings, Settings
+from controllers import DataController
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(project_id: str, file: UploadFile,
+                      apee_settings: Settings = Depends(get_settings)):
+    # validate the file properties
+    is_valid = DataController().validate_uploaded_file(file=file)
+
+    return is_valid
