f6f28bf99ebb37478118e9cf47f1d1f23e32b916
Mariam Ahmed <mariamalkaseh741@gmail.com>
2025-04-15 18:21:25 +0200

    edit main and config

--- src/stores/LLMEnums.py
+++ src/stores/LLMEnums.py
@@ -8,3 +8,16 @@ class OpenAIEnums(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+    class CoHereEnums(Enum):
+        SYSTEM = "SYSTEM"
+        USER = "USER"
+        ASSISTANT = "CHATBOT" 
+
+        DOCUMENT = "search_document"
+        Query = "search_query"
+
+
+    class DocumentTypeEnum(Enum):
+        DOCUMENT = "document"
+        QUERY = "query"
